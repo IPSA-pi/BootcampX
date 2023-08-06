69e5071d8063a6397ae66f5c90401f5c1cf33ab6
Ian Sebleius <sebeliusancira@gmail.com>
2023-08-06 12:42:04 -0700

    Sanitize data inputs for teachers and students queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,17 +9,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = argv[2];
-
-pool.query(`
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name;
-`)
+`;
+
+const cohortName = `%${process.argv[2]}%`;
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
